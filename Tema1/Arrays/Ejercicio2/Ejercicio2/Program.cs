773caccd18634868606e1347bcf6da681cf34276
ElianMateo <sanchezaguilarelianmateo@gmail.com>
2025-09-29 23:45:25 +0200

    Commit con ejercicio 2 realizado 29.09.2025v2 - Elian Mateo Sanchez Aguilar

---
+++ Tema1/Arrays/Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaramos objeto de tipo Random para poder tener "aleatoridad"
+            Random random = new Random();
+            //Declaracion del Array del 0 al 10
+            int[] frecuencia = new int[11];
+            //aqui uso el valor del random en un rango del 1 al 10 y segun el numero se incrementa el valor de la celda
+            for(int i = 0; i < 10000; i++)
+            {
+                int indice = random.Next(1, 11);
+                frecuencia[indice]++;
+            }
+            //Impresion
+            Console.WriteLine("FRECUENCIAS");
+            for(int i = 1;i < frecuencia.Length; i++)
+            {
+                Console.WriteLine("Numero "+i+" : "+frecuencia[i]);
+            }
+        }
+    }
+}
