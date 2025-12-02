054fec22307f2651af4aaeddf3a3460c8c10503b
ElianMateo_S_DAM <sanchezaguilarelianmateo@gmail.com>
2025-12-02 20:11:33 +0100

    Commit del ejercicio del 03.12.2025

--- Tema3/Ejercicio4_1/Ejercicio4_1/Vista/JuegoAlineacion.xaml.cs
+++ Tema3/Ejercicio4_1/Ejercicio4_1/Vista/JuegoAlineacion.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui.Animations;
+using System.Buffers.Text;
+
 namespace Ejercicio4_1.Vista;
 
 public partial class JuegoAlineacion : ContentPage
@@ -6,4 +9,47 @@ public partial class JuegoAlineacion : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	public void BtnStart_Clicked(object sender, EventArgs e)
+	{
+		lblCambiante.HorizontalOptions = LayoutOptions.Start;
+		lblCambiante.HorizontalTextAlignment = TextAlignment.Start;
+	}
+	public void BtnHorizontalCenter_Clicked(object sender, EventArgs e)
+	{
+        lblCambiante.HorizontalOptions = LayoutOptions.Center;
+        lblCambiante.HorizontalTextAlignment = TextAlignment.Center;
+    }
+	public void BtnHorizontalEnd_Clicked(object sender, EventArgs e)
+	{
+        lblCambiante.HorizontalOptions = LayoutOptions.End;
+        lblCambiante.HorizontalTextAlignment = TextAlignment.End;
+    }
+    public void BtnHorizontalFill_Clicked(object sender, EventArgs e)
+    {
+        lblCambiante.HorizontalOptions = LayoutOptions.Fill;
+
+    }
+
+    ////
+    public void BtnVerticalStart_Clicked(object sender, EventArgs e)
+    {
+        lblCambiante.VerticalOptions = LayoutOptions.Start;
+        lblCambiante.VerticalTextAlignment = TextAlignment.Start;
+    }
+    public void BtnVerticalCenter_Clicked(object sender, EventArgs e)
+    {
+        lblCambiante.VerticalOptions = LayoutOptions.Center;
+        lblCambiante.VerticalTextAlignment = TextAlignment.Center;
+    }
+    public void BtnVerticalEnd_Clicked(object sender, EventArgs e)
+    {
+        lblCambiante.VerticalOptions = LayoutOptions.End;
+        lblCambiante.VerticalTextAlignment = TextAlignment.End;
+    }
+    public void BtnVerticalFill_Clicked(object sender, EventArgs e)
+    {
+        lblCambiante.VerticalOptions = LayoutOptions.Fill;
+
+    }
 }
